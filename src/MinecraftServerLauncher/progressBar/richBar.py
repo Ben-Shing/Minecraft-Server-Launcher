7997677ec28e0a2cc4632ab75bec42e67f6c8320
Ben-Shing <58335543+Ben-Shing@users.noreply.github.com>
2025-03-28 23:02:41 +0800

    Adding progress bar feature(Not Done)

---
+++ src/MinecraftServerLauncher/progressBar/richBar.py
@@ -0,0 +1,40 @@
+from rich.progress import Progress
+
+class Bar():
+    
+    def __init__(self):
+        pass
+
+    def createWithTask(self, task: str):
+        """
+        Create a progress bar with a task.
+
+        :param task: The task name.
+        """
+        self.bar = Progress()
+        self.bar.add_task(task)
+
+    def createWithTotal(self, total: int):
+        """
+        Create a progress bar with the total number of items.
+
+        :param total: The total number of items.
+        """
+        self.bar = Progress()
+        self.id = self.bar.add_task('Progress', total=total)
+
+    def update(self, current: int):
+        """
+        Update the progress bar with the current number of items.
+
+        :param current: The current number of items.
+        """
+        self.bar.update(self.id, completed=current)
+
+    def complete(self):
+        """
+        Complete the progress bar.
+        """
+        self.bar.stop()
+        self.bar = None
+
